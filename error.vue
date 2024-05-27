b93d257c8695e3db87a767a8cfd57972a754a109
sandroardhi <sandrosaputra557@gmail.com>
2024-05-27 19:58:05 +0700

    error handling

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+    <section>
+        <h1 class="text-4xl font-semibold text-gray-700 dark:text-gray-200">{{ props.error.statusCode }}</h1>
+        <h1 class="text-3xl text-gray-700 dark:text-gray-200">{{ props.error.statusMessage }}</h1>
+        <NuxtLink to="/" class="px-2 py-1 dark:bg-gray-700 dark:text-gray-300 bg-gray-100 text-gray-700 mt-10 inline-block">Get back to home</NuxtLink>
+    </section>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+</script>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,67 +2,76 @@
   <article
     class="prose dark:prose-invert max-w-none prose-pre:bg-gray-200 dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
   >
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-16">
-        <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <!-- start not found slot -->
+      <template #not-found> 
+        <h1>Document not found :(</h1>
+        <p>This blog post could not be found. Please try another post.</p>
+      </template>
+      <!-- end not found slot -->
+
+      <!-- start doc slot -->
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-16">
+          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="col-span-2" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold">Table Of Contents</div>
+              <nav class="not-prose">
+                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div class="col-span-2" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold">
-              Table Of Contents
-            </div>
-            <nav class="not-prose">
-              <TocLinks :links="doc.body.toc.links" :active-id="activeId"/>
-            </nav>
-          </aside>
-        </div>
-      </div>
+      </template>
+      <!-- end doc slot -->
     </ContentDoc>
   </article>
 </template>
 
 <script setup>
-const activeId = ref(null)
+const activeId = ref(null);
 onMounted(() => {
   const callback = (entries) => {
     for (const entry of entries) {
-      if(entry.isIntersecting) {
-        activeId.value = entry.target.id
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id;
         break;
       }
-    };
-  }
+    }
+  };
   const observer = new IntersectionObserver(callback, {
     root: null,
-    threshold: 0.5
-  })
-  const elements = document.querySelectorAll('h2', 'h3')
-
-  elements.forEach(element => {
-    observer.observe(element)
+    threshold: 0.5,
   });
-})
+  const elements = document.querySelectorAll("h2", "h3");
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
 
 onBeforeUnmount(() => {
   const callback = (entries) => {
     for (const entry of entries) {
-      if(entry.isIntersecting) {
-        activeId.value = entry.target.id
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id;
         break;
       }
-    };
-  }
-  const elements = document.querySelectorAll('h2', 'h3')
+    }
+  };
+  const elements = document.querySelectorAll("h2", "h3");
   const observer = new IntersectionObserver(callback, {
     root: null,
-    threshold: 0.5
-  })
+    threshold: 0.5,
+  });
 
   for (const element of elements) {
-    observer.unobserve(element)
+    observer.unobserve(element);
   }
-})
+});
 </script>
 
 <style></style>
